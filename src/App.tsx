683efecb9bdd1e269e8112fba891542658603e21
caesaragen <caesaragen@gmail.com>
2022-09-28 12:40:10 +0300

    style select box

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { useState } from 'react'
+import { Select } from './Select'
 
+const options = [
+  { value: 1, label: 'One' },
+  { value: 2, label: 'Two' },
+  { value: 3, label: 'Three' },
+  { value: 4, label: 'Four' },
+  { value: 5, label: 'Five' },
+]
 
 function App() {
 
-  return <h1>Hello</h1>
+  return (
+    <Select options={options}/> 
+  )
 }
 
 export default App
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,6 +1,7 @@
+import styles from './Select.module.css';
 
 type SelectOption = {
-    value: string;
+    value: any;
     label: string;
 };
 type SelectProps = {
@@ -10,5 +11,19 @@ type SelectProps = {
 }
 
 export function Select({ value, onChange, options }: SelectProps) {
-    
+    return (
+        <div tabIndex={0} className={styles.container}>
+            <span className={styles.value}>Value </span>
+                <button className={styles["clear-button"]}>&times;</button>
+                <div className={styles.divider}></div>
+                <div className={styles.caret}></div>
+            <ul className={`${ styles.options } ${styles.show} `}>
+                {options.map((option) => (
+                    <li key={option.label} className={ styles.option }>
+                        {option.label}
+                    </li>
+                ))}
+                </ul>
+        </div>       
+    )
 }
